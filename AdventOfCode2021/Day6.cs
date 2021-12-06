ff1b33fffd783b7812c27490fe5cddacc3370eb6
Alexandre Charron <charronalexandre@hotmail.com>
2021-12-06 11:59:55 -0500

    solve day 6

---
+++ AdventOfCode2021/Day6.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    class Day6
+    {
+        public static void part1()
+        {
+            string[] numbers = File.ReadAllLines("day6.txt")[0].Split(',');
+
+            int[] lanternFish = new int[9];
+
+            for(int i = 0; i < numbers.Length; ++i)
+            {
+                lanternFish[int.Parse(numbers[i])]++;
+            }
+
+            for(int i = 0; i < 80; ++i)
+            {
+                int[] temp = new int[9];
+                temp[0] = lanternFish[1];
+                temp[1] = lanternFish[2];
+                temp[2] = lanternFish[3];
+                temp[3] = lanternFish[4];
+                temp[4] = lanternFish[5];
+                temp[5] = lanternFish[6];
+                temp[6] = lanternFish[7] + lanternFish[0];
+                temp[7] = lanternFish[8];
+                temp[8] = lanternFish[0];
+                lanternFish = temp;
+            }
+
+            int sum = lanternFish[0] + lanternFish[1] + lanternFish[2] + lanternFish[3] + lanternFish[4] + lanternFish[5] + lanternFish[6] + lanternFish[7] + lanternFish[8];
+
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+
+        public static void part2()
+        {
+            string[] numbers = File.ReadAllLines("day6.txt")[0].Split(',');
+
+            long[] lanternFish = new long[9];
+
+            for (int i = 0; i < numbers.Length; ++i)
+            {
+                lanternFish[int.Parse(numbers[i])]++;
+            }
+
+            for (int i = 0; i < 256; ++i)
+            {
+                long[] temp = new long[9];
+                temp[0] = lanternFish[1];
+                temp[1] = lanternFish[2];
+                temp[2] = lanternFish[3];
+                temp[3] = lanternFish[4];
+                temp[4] = lanternFish[5];
+                temp[5] = lanternFish[6];
+                temp[6] = lanternFish[7] + lanternFish[0];
+                temp[7] = lanternFish[8];
+                temp[8] = lanternFish[0];
+                lanternFish = temp;
+            }
+
+            long sum = lanternFish[0] + lanternFish[1] + lanternFish[2] + lanternFish[3] + lanternFish[4] + lanternFish[5] + lanternFish[6] + lanternFish[7] + lanternFish[8];
+
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Day5.part2();
+            Day6.part1();
         }
     }
 }
