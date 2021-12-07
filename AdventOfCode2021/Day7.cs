5b5e047880b7b2f47a72facd63247bffe71d951e
Alexandre Charron <charronalexandre@hotmail.com>
2021-12-07 00:37:36 -0500

    solve day 7

---
+++ AdventOfCode2021/Day7.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    class Day7
+    {
+        public static void part1()
+        {
+            string[] numbers = File.ReadAllLines("day7.txt")[0].Split(',');
+
+            int[] positions = new int[numbers.Length];
+
+            for (int i = 0; i < positions.Length; ++i)
+                positions[i] = int.Parse(numbers[i]);
+
+            List<int> costs = new List<int>();
+
+            for(int i = 0; i < 2000; ++i)
+            {
+                int cost = 0;
+
+                for (int j = 0; j < positions.Length; ++j)
+                    cost += Math.Abs(positions[j] - i);
+                costs.Add(cost);
+            }
+
+            costs.Sort();
+
+            Console.WriteLine(costs[0]);
+            Console.Read();
+        }
+
+        public static void part2()
+        {
+            string[] numbers = File.ReadAllLines("day7.txt")[0].Split(',');
+
+            int[] positions = new int[numbers.Length];
+
+            for (int i = 0; i < positions.Length; ++i)
+                positions[i] = int.Parse(numbers[i]);
+
+            List<int> costs = new List<int>();
+
+            for (int i = 0; i < 2000; ++i)
+            {
+                int cost = 0;
+
+                for (int j = 0; j < positions.Length; ++j)
+                {
+                    int abs = Math.Abs(positions[j] - i);
+                    cost += abs * (abs + 1) / 2;
+                }
+                    
+                costs.Add(cost);
+            }
+
+            costs.Sort();
+
+            Console.WriteLine(costs[0]);
+            Console.Read();
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Day6.part1();
+            Day7.part2();
         }
     }
 }
