a275dcaeb6784759fab45252256866949ecea3d6
Alexandre Charron <charronalexandre@hotmail.com>
2021-12-08 12:45:41 -0500

    don't use regex

--- AdventOfCode2021/Day8.cs
+++ AdventOfCode2021/Day8.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AdventOfCode2021
 {
@@ -14,16 +9,14 @@ namespace AdventOfCode2021
         {
             string[] lines = File.ReadAllLines("day8.txt");
 
-            Regex r = new Regex(@"(\w+) (\w+) (\w+) (\w+) (\w+) (\w+) (\w+) (\w+) (\w+) (\w+) \| (\w+) (\w+) (\w+) (\w+)");
-
             int count = 0;
             foreach(var line in lines)
             {
-                Match m = r.Match(line);
+                string[] digits = line.Split(new[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries);
 
-                for (int i = 11; i < 15; ++i)
+                for (int i = 10; i < digits.Length; ++i)
                 {
-                    int length = m.Groups[i].Value.Length;
+                    int length = digits[i].Length;
 
                     if (length == 2 || length == 3 || length == 4 || length == 7)
                         count++;
