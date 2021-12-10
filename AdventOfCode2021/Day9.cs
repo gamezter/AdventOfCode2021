dca1c367cdcba37afb55b7a75ecb2cd65b4dc388
Alexandre Charron <charronalexandre@hotmail.com>
2021-12-09 20:06:49 -0500

    solve day 9 part 2

--- AdventOfCode2021/Day9.cs
+++ AdventOfCode2021/Day9.cs
@@ -40,5 +40,73 @@ skip:               ;
             Console.WriteLine(sum);
             Console.Read();
         }
+
+        public static void part2()
+        {
+            string[] lines = File.ReadAllLines("day9.txt");
+            int maxY = lines.Length;
+            int maxX = lines[0].Length;
+
+            int[,] map = new int[maxX, maxY];
+
+            for (int y = 0; y < maxY; ++y)
+                for (int x = 0; x < maxX; ++x)
+                    map[x,y] = lines[y][x] - '0';
+
+            List<int> basins = new List<int>();
+
+            for (int y = 0; y < maxY; ++y)
+            {
+                for (int x = 0; x < maxX; ++x)
+                {
+                    int center = map[x,y];
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        var (dx, dy) = offsets[i];
+                        int nx = x + dx;
+                        int ny = y + dy;
+                        if (nx < 0 || nx >= maxX || ny < 0 || ny >= maxY)
+                            continue;
+
+                        if (map[nx, ny] <= center)
+                            goto skip;
+                    }
+                    //found low point
+
+                    int size = 1;
+
+                    Queue<(int, int, int)> toVisit = new Queue<(int, int, int)>();
+                    toVisit.Enqueue((x - 1, y, center));
+                    toVisit.Enqueue((x + 1, y, center)); 
+                    toVisit.Enqueue((x, y - 1, center));
+                    toVisit.Enqueue((x, y + 1, center));
+
+                    while(toVisit.Count > 0)
+                    {
+                        var (nx, ny, prev) = toVisit.Dequeue();
+                        if (nx < 0 || nx >= maxX || ny < 0 || ny >= maxY || map[nx, ny] == 9)
+                            continue;
+                        int curr = map[nx, ny];
+
+                        if (curr > prev)
+                        {
+                            map[nx, ny] = 9;
+                            size++;
+                            toVisit.Enqueue((nx - 1, ny, curr));
+                            toVisit.Enqueue((nx + 1, ny, curr));
+                            toVisit.Enqueue((nx, ny - 1, curr));
+                            toVisit.Enqueue((nx, ny + 1, curr));
+                        }
+                    }
+                    basins.Add(size);
+                    skip:;
+                }
+            }
+
+            basins.Sort();
+
+            Console.WriteLine(basins[basins.Count - 3] * basins[basins.Count - 2] * basins[basins.Count - 1]);
+            Console.Read();
+        }
     }
 }
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Day9.part1();
+            Day9.part2();
         }
     }
 }
