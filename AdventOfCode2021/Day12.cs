e2b0e4475ce0c2eb329f239a6b20ef4746adad59
Alexandre Charron <charronalexandre@hotmail.com>
2021-12-13 20:16:11 -0500

    solve day 12 part 2

--- AdventOfCode2021/Day12.cs
+++ AdventOfCode2021/Day12.cs
@@ -60,5 +60,62 @@ namespace AdventOfCode2021
             Console.WriteLine(count);
             Console.Read();
         }
+
+        public static void part2()
+        {
+            string[] lines = File.ReadAllLines("day12.txt");
+
+            List<(char, char)> links = new List<(char, char)>();
+
+            foreach (var line in lines)
+            {
+                string[] nodes = line.Split('-');
+                links.Add((nodes[0][0], nodes[1][0]));
+            }
+
+            int count = 0;
+
+            Stack<char> temp = new Stack<char>();
+            temp.Push('s');
+
+            visit(temp, false);
+
+            void visit(Stack<char> visited, bool already2)
+            {
+                char lastVisited = visited.Peek();
+                foreach (var (a, b) in links)
+                {
+                    char other;
+                    if (a == lastVisited)
+                        other = b;
+                    else if (b == lastVisited)
+                        other = a;
+                    else continue;
+
+                    bool alreadyThere = char.IsLower(other) && visited.Contains(other);
+
+                    if (other == 's')
+                        continue;
+
+                    if (alreadyThere && already2)
+                        continue;
+
+                    if (other == 'e')
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        visited.Push(other);
+                        visit(visited, alreadyThere || already2);
+                    }
+                }
+
+                visited.Pop();
+            }
+
+            Console.WriteLine(count);
+            Console.Read();
+        }
     }
 }
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Day12.part1();
+            Day12.part2();
         }
     }
 }
