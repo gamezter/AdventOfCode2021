3e7b63662107a01dba8e7ed0496208746e6134da
Alexandre Charron <charronalexandre@hotmail.com>
2021-12-13 20:02:59 -0500

    solve day 12 part 1

---
+++ AdventOfCode2021/Day12.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    class Day12
+    {
+        public static void part1()
+        {
+            string[] lines = File.ReadAllLines("day12.txt");
+
+            List<(char, char)> links = new List<(char, char)>();
+
+            foreach(var line in lines)
+            {
+                string[] nodes = line.Split('-');
+                links.Add((nodes[0][0], nodes[1][0]));
+            }
+
+            int count = 0;
+
+            Stack<char> temp = new Stack<char>();
+            temp.Push('s');
+
+            visit(temp);
+
+            void visit(Stack<char> visited)
+            {
+                char lastVisited = visited.Peek();
+                foreach (var (a, b) in links)
+                {
+                    char other;
+                    if (a == lastVisited)
+                        other = b;
+                    else if (b == lastVisited)
+                        other = a;
+                    else continue;
+
+                    if (char.IsLower(other) && visited.Contains(other))
+                        continue;
+
+                    if(other == 'e')
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        visited.Push(other);
+                        visit(visited);
+                    }
+                }
+
+                visited.Pop();
+            }
+
+            Console.WriteLine(count);
+            Console.Read();
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Day11.part2();
+            Day12.part1();
         }
     }
 }
