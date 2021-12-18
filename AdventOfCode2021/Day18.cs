8450c5435d06b09f44f50f7a80515cf45ecd94b7
Alexandre Charron <charronalexandre@hotmail.com>
2021-12-18 17:43:05 -0500

    Solve day 18 part 1

---
+++ AdventOfCode2021/Day18.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    class Day18
+    {
+        public static void part1()
+        {
+            string[] lines = File.ReadAllLines("day18.txt");
+
+            List<char> sum = new List<char>();
+
+            for (int i = 0; i < lines[0].Length; ++i)
+            {
+                char c = lines[0][i];
+                if (c == ',')
+                    continue;
+
+                if (c == '[' || c == ']')
+                    sum.Add(c);
+                else
+                    sum.Add((char)(c - '0'));
+            }
+
+
+            for(int i = 1; i < lines.Length; ++i)
+            {
+                sum = Add(sum, lines[i]);
+
+                restart:
+                if (Explode(sum))
+                    goto restart;
+                if (Split(sum))
+                    goto restart;
+            }
+
+            int index = 0;
+
+            int magnitude()
+            {
+                while(sum[index] == ']')
+                    index++;
+
+                if (sum[index] < 10)
+                    return sum[index++];
+                else
+                {
+                    index++;
+                    return 3 * magnitude() + 2 * magnitude();
+                }
+            }
+
+            Console.WriteLine(magnitude());
+            Console.Read();
+        }
+
+        public static void part2()
+        {
+            string[] lines = File.ReadAllLines("day18.txt");
+
+            List<char> sum = new List<char>();
+
+            for (int i = 0; i < lines[0].Length; ++i)
+            {
+                char c = lines[0][i];
+                if (c == ',')
+                    continue;
+
+                if (c == '[' || c == ']')
+                    sum.Add(c);
+                else
+                    sum.Add((char)(c - '0'));
+            }
+
+
+            for (int i = 1; i < lines.Length; ++i)
+            {
+                sum = Add(sum, lines[i]);
+
+                restart:
+                if (Explode(sum))
+                    goto restart;
+                if (Split(sum))
+                    goto restart;
+            }
+
+            int index = 0;
+
+            int magnitude()
+            {
+                while (sum[index] == ']')
+                    index++;
+
+                if (sum[index] < 10)
+                    return sum[index++];
+                else
+                {
+                    index++;
+                    return 3 * magnitude() + 2 * magnitude();
+                }
+            }
+
+            Console.WriteLine(magnitude());
+            Console.Read();
+        }
+
+        static List<char> Add(List<char> left, string right)
+        {
+            List<char> ret = new List<char> { '[' };
+            ret.AddRange(left);
+            for (int i = 0; i < right.Length; ++i)
+            {
+                char c = right[i];
+                if (c == ',')
+                    continue;
+
+                if (c == '[' || c == ']')
+                    ret.Add(c);
+                else
+                    ret.Add((char)(c - '0'));
+            }
+            ret.Add(']');
+            return ret;
+        }
+
+        static bool Explode(List<char> sum)
+        {
+            int depth = 0;
+            for(int i = 0; i < sum.Count; ++i)
+            {
+                if (sum[i] == '[')
+                    depth++;
+                else if (sum[i] == ']')
+                    depth--;
+
+                if(depth == 5)
+                {
+                    int leftIndex = sum.FindLastIndex(i, i, c => c != '[' && c != ']');
+                    if(leftIndex != -1)
+                        sum[leftIndex] = (char)(sum[leftIndex] + sum[i + 1]);
+
+                    int rightIndex = sum.FindIndex(i + 3, c => c != '[' && c != ']');
+                    if (rightIndex != -1)
+                        sum[rightIndex] = (char)(sum[rightIndex] + sum[i + 2]);
+
+                    sum[i] = (char)0;
+                    sum.RemoveRange(i + 1, 3);
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool Split(List<char> sum)
+        {
+            for (int i = 0; i < sum.Count; ++i)
+            {
+                if (sum[i] != '[' && sum[i] != ']' && sum[i] > 9)
+                {
+                    int value = sum[i];
+                    int left = value / 2;
+                    int right = value - left;
+                    sum[i] = '[';
+                    sum.InsertRange(i + 1, new[] { (char)left, (char)right, ']' });
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Day17.part2();
+            Day18.part2();
         }
     }
 }
